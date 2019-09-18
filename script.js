9aced8c5dc7becaf93166ac82eb0a7faa9a5a9b9
Jonathan Goldfuss <Jonathan@Jonathans-Air-3.fios-router.home>
2019-09-18 16:28:02 -0400

    fixed a sequencing issue

--- script.js
+++ script.js
@@ -9,23 +9,23 @@ const greenSound = new Audio("./sound/green.mp3");
 const yellowSound = new Audio("./sound/yellow.mp3");
 const redSound = new Audio("./sound/red.mp3");
 const blueSound = new Audio("./sound/blue.mp3");
+const buzz = new Audio("./sound/buzz.mp3");
 
 // Randomizing the buttonPattern() function so game is different every time
 function randomizePattern() {
   let buttons = ["green", "yellow", "red", "blue"];
-  for (let i = 0; i < 50; i++) {
+  for (let i = 0; i < 100; i++) {
     buttonPattern.push(buttons[Math.floor(Math.random() * buttons.length)]);
   }
 }
 randomizePattern();
-// console.log(buttonPattern);
+console.log(buttonPattern);
 let buttonPatternCopyC = [...buttonPattern];
 let buttonPatternCopyU = [...buttonPattern];
-// console.log(buttonPatternCopy);
 
 // Setting some variables for global use
-const buttons = document.querySelectorAll(".button");
-const buttonsContainer = document.querySelector(".container");
+// const buttons = document.querySelectorAll(".button");
+// const buttonsContainer = document.querySelector(".container");
 
 const greenButton = document.querySelector("#green");
 const yellowButton = document.querySelector("#yellow");
@@ -63,27 +63,19 @@ function flashBlue() {
 }
 
 // Starting game action
-
 // set to Start button in html
-
 function computerTurn(n) {
   console.log(`Level: ${n}`);
+  // Setting countdown to stop at 0.
   if (n <= 0) {
     level = arrayHolder.length;
     for (let i = 0; arrayHolder.length > 0; i++) {
-      let last = arrayHolder.pop();
-      buttonPatternCopyC.unshift(last);
+      buttonPatternCopyC.unshift(arrayHolder.pop());
     }
-    console.log(buttonPatternCopyC);
-    console.log(arrayHolder);
     return;
+    // Otherwise, play the first button in the list.
   } else {
-    console.log(buttonPattern);
-    console.log(buttonPatternCopyC);
-
     let buttonIdStr = buttonPatternCopyC.shift();
-    console.log(buttonIdStr);
-
     if (buttonIdStr === "green") {
       flashGreen();
     } else if (buttonIdStr === "yellow") {
@@ -94,17 +86,17 @@ function computerTurn(n) {
       flashBlue();
     }
     arrayHolder.push(buttonIdStr);
-    console.log(arrayHolder);
-
+    // Then go on to the next...(recursively)
     setTimeout(function() {
       computerTurn(n - 1);
     }, 500);
   }
 }
 
-// Listening for user input, adding to user array
+// Listening for user click...
+const buttonsContainer = document.querySelector(".container");
 buttonsContainer.addEventListener("click", function(evt) {
-  // Play sounds and "flash" button
+  // Then play sounds and "flash" button each time
   let buttonIdStr = evt.target.id;
 
   if (buttonIdStr === "green") {
@@ -116,41 +108,43 @@ buttonsContainer.addEventListener("click", function(evt) {
   } else if (buttonIdStr === "blue") {
     flashBlue();
   }
+  // Push the clicked button id to an array
   userPattern.push(buttonIdStr);
-  // console.log(userPattern);
+  // ... then check if it's correct.
   checkContinue(userPattern);
 });
 
 // Check continue
-function checkContinue(array) {
-  if (array.length === level) {
-    let compPatternPlayed = [];
-    for (let i = 0; i < array.length; i++) {
-      compPatternPlayed.push(buttonPatternCopyU.shift());
-    }
-    if (JSON.stringify(array) !== JSON.stringify(compPatternPlayed)) {
-      alert("wrong!");
-      // gameOver();
-    } else {
-      level++;
-      setTimeout(function() {
-        computerTurn(level);
-      }, 1000);
-      console.log(userPattern);
-      for (let i = 0; userPattern.length > 0; i++) {
-        let last = userPattern.pop();
-        buttonPatternCopyU.unshift(last);
-        console.log(compPatternPlayed);
-      }
-      console.log(userPattern);
-    }
+function checkContinue(userArray) {
+  let tempArray = [...userArray];
+  let compPatternPlayed = [];
+  // Push the computer order (so far) into an array...
+  for (let i = 0; i < tempArray.length; i++) {
+    compPatternPlayed.push(buttonPatternCopyU.shift());
+  }
+  // ... check if user's input is right.
+  if (JSON.stringify(tempArray) !== JSON.stringify(compPatternPlayed)) {
+    gameOver();
+    return;
+    // If it is, check if they've clicked enough buttons to level up.
+  } else if (tempArray.length === level) {
+    level++;
+    userPattern = [];
+    setTimeout(function() {
+      computerTurn(level);
+    }, 1000);
+    // If not, clear the arrays and wait for more input.
+  } else {
+    compPatternPlayed = [];
+  }
+  for (let i = 0; tempArray.length > 0; i++) {
+    buttonPatternCopyU.unshift(tempArray.pop());
   }
 }
 
-// gameOver() {
-//   ;
-// }
-
-// winGame() {
-
-// }
+function gameOver() {
+  buzz.play();
+  alert("Whoops! Wrong button. Click 'start' to play again.");
+  location.reload(true);
+  randomizePattern();
+}
