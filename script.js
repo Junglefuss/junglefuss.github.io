ee8fdcddba9ce02ed8dc34e732122020de96ff7e
Jonathan Goldfuss <Jonathan@Jonathans-Air-3.fios-router.home>
2019-09-17 22:10:34 -0400

    pulling my hair out

--- script.js
+++ script.js
@@ -1,101 +1,133 @@
 // Creating the array for the game and the user:
 const buttonPattern = [];
-const userPattern = [];
-const level = 1;
+let userPattern = [];
+let level = 1;
 
 // Adding sounds
-const oneSound = new Audio("./sound/green.mp3");
-const twoSound = new Audio("./sound/yellow.mp3");
-const threeSound = new Audio("./sound/red.mp3");
-const fourSound = new Audio("./sound/blue.mp3");
+const greenSound = new Audio("./sound/green.mp3");
+const yellowSound = new Audio("./sound/yellow.mp3");
+const redSound = new Audio("./sound/red.mp3");
+const blueSound = new Audio("./sound/blue.mp3");
 
-// Randomizing the pattern so game is different every time
+// Randomizing the buttonPattern() function so game is different every time
 function randomizePattern() {
-  let buttons = ["one", "two", "three", "four"];
-  for (let i = 0; i < 20; i++) {
+  let buttons = ["green", "yellow", "red", "blue"];
+  for (let i = 0; i < 50; i++) {
     buttonPattern.push(buttons[Math.floor(Math.random() * buttons.length)]);
   }
 }
 randomizePattern();
 console.log(buttonPattern);
 
+// Setting some variables for global use
+const buttons = document.querySelectorAll(".button");
+const buttonsContainer = document.querySelector(".container");
+
+const greenButton = document.querySelector("#green");
+const yellowButton = document.querySelector("#yellow");
+const redButton = document.querySelector("#red");
+const blueButton = document.querySelector("#blue");
+
+// Making the buttons flash and play noise
+function flashGreen() {
+  greenButton.style.backgroundColor = "rgba(0, 128, 0, 1)";
+  greenSound.play();
+  setTimeout(function() {
+    greenButton.style.backgroundColor = "rgba(0, 128, 0, 0.3)";
+  }, 300);
+}
+function flashYellow() {
+  yellowButton.style.backgroundColor = "rgba(255, 255, 0, 1)";
+  yellowSound.play();
+  setTimeout(function() {
+    yellowButton.style.backgroundColor = "rgba(255, 255, 0, 0.3)";
+  }, 300);
+}
+function flashRed() {
+  redButton.style.backgroundColor = "rgba(255, 0, 0, 1)";
+  redSound.play();
+  setTimeout(function() {
+    redButton.style.backgroundColor = "rgba(255, 0, 0, 0.3)";
+  }, 300);
+}
+function flashBlue() {
+  blueButton.style.backgroundColor = "rgba(0, 0, 255, 1)";
+  blueSound.play();
+  setTimeout(function() {
+    blueButton.style.backgroundColor = "rgba(0, 0, 255, 0.3)";
+  }, 300);
+}
+
 // Starting game action
 
-function startGame() {
-  flashAndSound();
-}
+// set to Start button
+function computerTurn(n) {
+  level++;
+  console.log(n);
+  if (n <= 0) {
+    userPattern = [];
+    return;
+  } else {
+    // for (let i = 0; i <= userPattern.length; i++) {
+    // let compPatternSoFar = buttonPattern.slice(0, userPattern.length + 1);
+    let x = 0;
+    let buttonIdStr = buttonPattern[x];
+    console.log(buttonIdStr);
 
-// Listening for user input, adding to user array
+    if (buttonIdStr === "green") {
+      flashGreen();
+    } else if (buttonIdStr === "yellow") {
+      flashYellow();
+    } else if (buttonIdStr === "red") {
+      flashRed();
+    } else if (buttonIdStr === "blue") {
+      flashBlue();
+    } else return;
 
-let buttons = document.querySelectorAll(".button");
-let buttonsDiv = document.querySelector(".container");
-
-function userInput() {
-  buttonsDiv.addEventListener("click", function(evt) {
-    // Check continue
-    userPattern.push(evt.target.id);
-    let buttonPatternSoFar = buttonPattern.slice(0, userPattern.length);
-
-    // else {
-    //   XXXXXXXXXXXXXXXXXXXXXXXX;
-    // }
-
-    // evt.target.style.backgroundColor = 1;
-
-    if (evt.target.id === "one") {
-      oneSound.play();
-      evt.target.style.backgroundColor = "rgba(0, 128, 0, 1)";
-      setTimeout(function() {
-        evt.target.style.backgroundColor = "rgba(0, 128, 0, 0.3)";
-      }, 300);
-      console.log(evt.target.style.backgroundColor);
-    } else if (evt.target.id === "two") {
-      twoSound.play();
-      evt.target.style.backgroundColor = "rgba(255, 255, 0, 1)";
-      setTimeout(function() {
-        evt.target.style.backgroundColor = "rgba(255, 255, 0, 0.3)";
-      }, 300);
-    } else if (evt.target.id === "three") {
-      threeSound.play();
-      evt.target.style.backgroundColor = "rgba(255, 0, 0, 1)";
-      setTimeout(function() {
-        evt.target.style.backgroundColor = "rgba(255, 0, 0, 0.3)";
-      }, 300);
-    } else if (evt.target.id === "four") {
-      fourSound.play();
-      evt.target.style.backgroundColor = "rgba(0, 0, 255, 1)";
-      setTimeout(function() {
-        evt.target.style.backgroundColor = "rgba(0, 0, 255, 0.3)";
-      }, 300);
-    }
-
-    if (JSON.stringify(userPattern) !== JSON.stringify(buttonPatternSoFar)) {
-      alert("loser!");
-      // gameOver();
-    }
-  });
-}
-userInput();
-
-// Making buttons flash and play sound
-function flashColor() {}
-
-// Setting the flash to a pattern
-function playPattern([]) {
-  for (let i = 0; i < buttonPattern.length; i++) {
-    let currentPattern = [];
-    let userTurns = userPattern.length;
-    currentPattern.push(buttonPattern[userTurns + 1]);
+    setTimeout(function() {
+      x++;
+      computerTurn(n - 1);
+      console.log(x);
+    }, 300);
   }
 }
+// }
 
-let greenButton = document.getElementById("one");
-let yellowButton = document.getElementById("two");
-let redButton = document.getElementById("three");
-let blueButton = document.getElementById("four");
+// Listening for user input, adding to user array
+buttonsContainer.addEventListener("click", function(evt) {
+  // Play sounds and "flash" button
+  if (evt.target.id === "green") {
+    flashGreen();
+  } else if (evt.target.id === "yellow") {
+    flashYellow();
+  } else if (evt.target.id === "red") {
+    flashRed();
+  } else if (evt.target.id === "blue") {
+    flashBlue();
+  }
+  userPattern.push(evt.target.id);
+  console.log(userPattern);
 
-let testBox = document.querySelector(".box");
+  // Check continue
+  let compPatternSoFar = buttonPattern.slice(0, userPattern.length);
+  console.log(compPatternSoFar);
+  if (userPattern.length === compPatternSoFar.length) {
+    if (JSON.stringify(userPattern) !== JSON.stringify(compPatternSoFar)) {
+      alert("wrong!");
+      // gameOver();
+    } else {
+      setTimeout(function() {
+        computerTurn(userPattern.length + 1);
+      }, 1000);
+    }
+    console.log(userPattern.length);
+  }
+});
 
 // gameOver() {
 //   ;
+// }
+
+// winGame() {
+
 // }
